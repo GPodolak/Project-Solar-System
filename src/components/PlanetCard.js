6a6492f93a951d2d041a5b207b36518fc7a3dd86
Guilherme Podolak <podolak12345@gmail.com>
2022-06-28 18:23:33 -0300

    PlanetCards

--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 class planetCards extends React.Component {
-  // render() {
-  //   return (
-  //     <div data-testid="planet-card">
-
-  //     </div>
-  //   );
-  // }
+  render() {
+    return (
+      <div data-testid="planet-card">
+        <h3 data-testid="planet-name">{planetName}</h3>
+        <img className="imgPlanet" src={ planetImage } alt={ `Planeta ${planetName}` } />
+      </div>
+    );
+  }
 }
 export default planetCards;
