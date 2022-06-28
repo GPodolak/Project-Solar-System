d2355f6a67efd8cec44b0ea8b71797235b8c0c00
Guilherme Podolak <podolak12345@gmail.com>
2022-06-27 22:05:42 -0300

    requisito 1 teste

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import Header from './components/Header';
+
+class Header extends React.Component {
+  render() {
+    return (
+    //   <>
+      <Header>
+        <h1>Sistema Solar</h1>
+      </Header>
+    // </>
+    );
+  }
+}
+export default Header;
