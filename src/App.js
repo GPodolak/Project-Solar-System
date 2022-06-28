e718f0ddd62f6f694a742eeae469987f09074155
Guilherme Podolak <podolak12345@gmail.com>
2022-06-28 20:38:58 -0300

    requisito planetcard + correção

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import SolarSystem from './components/SolarSystem';
 import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
 
 class App extends React.Component {
   render() {
--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import propTypes from 'prop-types';
 
 class planetCards extends React.Component {
-  // render() {
-  //   return (
-  //     // <div data-testid="planet-card">
-  //     //   <h3 data-testid="planet-name">{planetName}</h3>
-  //     //   <img className="imgPlanet" src={ planetImage } alt={ `Planeta ${planetName}` } />
-  //     // </div>
-  //   // );
-  // }
+  render() {
+    const { planetName, planetImage } = this.props;
+    return (
+      <div data-testid="planet-card">
+        <h3 data-testid="planet-name">{planetName}</h3>
+        <img className="imgPlanet" src={ planetImage } alt={ `Planeta ${planetName}` } />
+      </div>
+    );
+  }
 }
+planetCards.propTypes = {
+  planetName: propTypes.string.isRequired,
+  planetImage: propTypes.string.isRequired,
+};
 export default planetCards;
