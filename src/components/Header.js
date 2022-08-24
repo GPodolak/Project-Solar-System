5db1f5a98ee52b78ae77b072dc2e5e46b8d2c6a5
Guilherme Podolak <podolak12345@gmail.com>
2022-08-23 21:24:59 -0300

    Alteração para subir no perfil Pessoal

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import './App.css';
 
 class Header extends React.Component {
   render() {
     return (
     //   <>
       <header>
-        <h1>Sistema Solar</h1>
+        <h1 className="MainTitle">
+          Sistema Solar
+        </h1>
       </header>
     // </>
     );
--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -5,7 +5,7 @@ class planetCards extends React.Component {
   render() {
     const { planetName, planetImage } = this.props;
     return (
-      <div data-testid="planet-card">
+      <div className="SubTitle" data-testid="planet-card">
         <h3 data-testid="planet-name">{planetName}</h3>
         <img className="imgPlanet" src={ planetImage } alt={ `Planeta ${planetName}` } />
       </div>
